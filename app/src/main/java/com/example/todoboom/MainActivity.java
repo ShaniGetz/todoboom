00e0bead8cd08c3ae80dbc8323198d10f7a9f5c5
shanigetz <shani.getz@mail.huji.ac.il>
2020-04-06 18:28:19 +0300

    initial commit

---
+++ app/src/main/java/com/example/todoboom/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.todoboom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText inputField;
+    TextView textView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputField = findViewById(R.id.input_field);
+        textView = findViewById(R.id.text);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(inputField.getText());
+                inputField.setText("");
+            }
+        });
+
+        }
+    }
